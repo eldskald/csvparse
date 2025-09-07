cd462c5b5b7baeafad84aa9b49478812c1965900
eldskald <rafaelbordoni00@gmail.com>
2025-09-07 17:28:38 -0300

    Add tests

--- tests/test.c
+++ tests/test.c
@@ -1,7 +1,217 @@
 #include <csvparse.h>
+#include <stdio.h>
+#include <string.h>
+
+#define GREEN  "\x1b[32m"
+#define RED    "\x1b[31m"
+#define YELLOW "\x1b[33m"
+#define RESET  "\x1b[0m"
+
+void expect(char expectation, int *errors) {
+    if (expectation) {
+        printf(GREEN "Passed\n");
+    } else {
+        printf(RED "Failed\n");
+        (*errors)++;
+    }
+}
+
+void test1(int *errors) {
+    printf(RESET "--- 01.csv with comma and empty cell ------------------\n");
+
+    int err = 0;
+    const char ***data = csvparse("tests/01.csv", &err);
+
+    printf(RESET
+           "Got err = %d, expected 0 ", err);
+    expect(err == 0, errors);
+
+    printf(RESET
+           "Got csvrowscount(data) = %d, expected 2 ", csvrowscount(data));
+    expect(csvrowscount(data) == 2, errors);
+
+    printf(RESET
+           "Got csvrowlen(data[0]) = %d, expected 3 ", csvrowlen(data[0]));
+    expect(csvrowlen(data[0]) == 3, errors);
+
+    printf(RESET
+           "Got csvrowlen(data[1]) = %d, expected 4 ", csvrowlen(data[0]));
+    expect(csvrowlen(data[1]) == 4, errors);
+
+    printf(RESET
+           "\nData points obtained:\n%s, %s, %s\n%s, %s, %s, %s",
+           data[0][0], data[0][1], data[0][2],
+           data[1][0], data[1][1], data[1][2], data[1][3]);
+    printf(RESET
+           "\nExpected:\ntest1, 123, with comma, at end of row"
+           "\ntest2, , with, comma, aaa\n");
+    expect(!strcmp(data[0][0], "test1") &&
+           !strcmp(data[0][1], "123") &&
+           !strcmp(data[0][2], "with comma, at end of row") &&
+           !strcmp(data[1][0], "test2") &&
+           !strcmp(data[1][1], "") &&
+           !strcmp(data[1][2], "with, comma") &&
+           !strcmp(data[1][3], "aaa"),
+           errors);
+
+    printf(RESET "-------------------------------------------------------\n\n");
+    csvfree(data);
+}
+
+void test2(int *errors) {
+    printf(RESET "--- 02.csv with empty rows and cells ------------------\n");
+
+    int err = 0;
+    const char ***data = csvparse("tests/02.csv", &err);
+
+    printf(RESET
+           "Got err = %d, expected 0 ", err);
+    expect(err == 0, errors);
+
+    printf(RESET
+           "Got csvrowscount(data) = %d, expected 5 ", csvrowscount(data));
+    expect(csvrowscount(data) == 5, errors);
+
+    printf(RESET
+           "Got csvrowlen(data[0]) = %d, expected 1 ", csvrowlen(data[0]));
+    expect(csvrowlen(data[0]) == 1, errors);
+
+    printf(RESET
+           "Got csvrowlen(data[1]) = %d, expected 3 ", csvrowlen(data[0]));
+    expect(csvrowlen(data[1]) == 3, errors);
+
+    printf(RESET
+           "Got csvrowlen(data[2]) = %d, expected 1 ", csvrowlen(data[0]));
+    expect(csvrowlen(data[2]) == 1, errors);
+
+    printf(RESET
+           "Got csvrowlen(data[3]) = %d, expected 1 ", csvrowlen(data[0]));
+    expect(csvrowlen(data[3]) == 1, errors);
+
+    printf(RESET
+           "Got csvrowlen(data[4]) = %d, expected 4 ", csvrowlen(data[0]));
+    expect(csvrowlen(data[4]) == 4, errors);
+
+    printf(RESET
+           "\nData points obtained:\n%s\n%s, %s, %s\n%s\n%s\n%s, %s, %s, %s",
+           data[0][0],
+           data[1][0], data[1][1], data[1][2],
+           data[2][0],
+           data[3][0],
+           data[4][0], data[4][1], data[4][2], data[4][3]);
+    printf(RESET
+           "\nExpected:\n\nnon, empty, column\n\n\nnon, , empty, again\n");
+    expect(!strcmp(data[0][0], "") &&
+           !strcmp(data[1][0], "non") &&
+           !strcmp(data[1][1], "empty") &&
+           !strcmp(data[1][2], "column") &&
+           !strcmp(data[2][0], "") &&
+           !strcmp(data[3][0], "") &&
+           !strcmp(data[4][0], "non") &&
+           !strcmp(data[4][1], "") &&
+           !strcmp(data[4][2], "empty") &&
+           !strcmp(data[4][3], "again"),
+           errors);
+
+    printf(RESET "-------------------------------------------------------\n\n");
+    csvfree(data);
+}
+
+void test3(int *errors) {
+    printf(RESET "--- 03.csv error due to quotation marks ---------------\n");
+
+    int err = 0;
+    const char ***data = csvparse("tests/03.csv", &err);
+
+    printf(RESET "Got error = %d, expected 2 ", err);
+    expect(err == 2, errors);
+
+    printf(RESET "Returned data expected to be null ");
+    expect(data == NULL, errors);
+
+    printf(RESET "-------------------------------------------------------\n\n");
+    csvfree(data);
+}
+
+void test4(int *errors) {
+    printf(RESET "--- 04.csv error due to cell beig too long ------------\n");
+
+    int err = 0;
+    const char ***data = csvparse("tests/04.csv", &err);
+
+    printf(RESET "Got error = %d, expected 3 ", err);
+    expect(err == 3, errors);
+
+    printf(RESET "Returned data expected to be null ");
+    expect(data == NULL, errors);
+
+    printf(RESET "-------------------------------------------------------\n\n");
+    csvfree(data);
+}
+
+void test5(int *errors) {
+    printf(RESET "--- 05.csv error due to row having too many cells -----\n");
+
+    int err = 0;
+    const char ***data = csvparse("tests/05.csv", &err);
+
+    printf(RESET "Got error = %d, expected 4 ", err);
+    expect(err == 4, errors);
+
+    printf(RESET "Returned data expected to be null ");
+    expect(data == NULL, errors);
+
+    printf(RESET "-------------------------------------------------------\n\n");
+    csvfree(data);
+}
+
+void test6(int *errors) {
+    printf(RESET "--- 06.csv error due to too many rows -----------------\n");
+
+    int err = 0;
+    const char ***data = csvparse("tests/06.csv", &err);
+
+    printf(RESET "Got error = %d, expected 5 ", err);
+    expect(err == 5, errors);
+
+    printf(RESET "Returned data expected to be null ");
+    expect(data == NULL, errors);
+
+    printf(RESET "-------------------------------------------------------\n\n");
+    csvfree(data);
+}
+
+void test7(int *errors) {
+    printf(RESET "--- inexistent.csv error to open file -----------------\n");
+
+    int err = 0;
+    const char ***data = csvparse("inexistent.csv", &err);
+
+    printf(RESET "Got error = %d, expected 1 ", err);
+    expect(err == 1, errors);
+
+    printf(RESET "Returned data expected to be null ");
+    expect(data == NULL, errors);
+
+    printf(RESET "-------------------------------------------------------\n\n");
+    csvfree(data);
+}
 
 int main() {
-    int size = 0;
-    csvparse("test/test1.csv", &size);
-    return 0;
+    int errors = 0;
+    
+    printf(YELLOW "RUNNING TESTS...\n\n");
+    test1(&errors);
+    test2(&errors);
+    test3(&errors);
+    test4(&errors);
+    test5(&errors);
+    test6(&errors);
+    test7(&errors);
+    printf(YELLOW "RESULTS: %d failure", errors);
+    if (errors != 1) printf("s");
+    if (errors > 0) printf(RED "\nFAILED\n");
+    else printf(GREEN "\nPASSED\n");
+
+    return errors > 0;
 }
